6162edccd31e8751efda2aef4f6499bcfb981718
j1ngk3 <34380697+j1ngk3@users.noreply.github.com>
2026-09-04 10:39:17 -0400

    test: change to zip file download, handle zip

--- tests/default/test_default.py
+++ tests/default/test_default.py
@@ -2,7 +2,8 @@ import pytest
 
 def test_manual_conversion(host):
     msg = "Relocating MOOV atom to start of file"
-    command = """curl -O https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4 && \
+    command = """curl -O https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4.zip && \
+                 unzip BigBuckBunny_320x180.mp4.zip && \
                  /sickbeard_mp4_automator/venv/bin/python /sickbeard_mp4_automator/manual.py -i "./BigBuckBunny_320x180.mp4" -a -nd -nt"""
 
     cmd = host.run(command)
